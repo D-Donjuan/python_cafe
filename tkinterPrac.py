09e20441ab69e522c18fafce60a67ce208198532
Joseph Santiago <curlyjrs92@gmail.com>
2022-10-12 17:11:24 -0500

    new window

--- tkinterPrac.py
+++ tkinterPrac.py
@@ -1,16 +1,28 @@
 from tkinter import *
+from tkinter import ttk
 
 window = Tk()
-
-window.geometry("510x660")
+window.title("Python Cafe")
+# window.geometry("0x0")
 bg = PhotoImage(file="LandingWithButton.png")
 
 canvasStart = Canvas(window, width=510, height=660)
 canvasStart.pack(fill="both", expand=True)
 
+
+def openNewWindow():
+    newWindow = Toplevel(window)
+    newWindow.geometry("200x200")
+
+
+def close():
+    win.quit()
+
+
 canvasStart.create_image(0, 0, image=bg, anchor="nw")
 
-orderButton = Button(window, text="Order Here")
+orderButton = Button(window, text="Order Here",
+                     bg='tan', command=openNewWindow)
 orderButton_canvas = canvasStart.create_window(
     205, 547, anchor="nw", window=orderButton)
 
